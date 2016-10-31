6b83cbbb399a5aeb783d702adb1e7c2a096f2fd9
brit1600 <brit1600@student.nhl.nl>
2016-10-31 21:15:25 +0100

    Versie 0.1

---
+++ Assets/Scripts/CarLapCounter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CarLapCounter : MonoBehaviour {
+
+	public TrackLapTrigger first;
+    public Text P1Lap;
+	TrackLapTrigger next;
+    public Text P1Win;
+    public Text P2Win;
+	int lap;
+   
+	// Use this for initialization
+	void Start () {
+		lap = 0;
+		SetNextTrigger(first);
+		UpdateText();
+    }
+
+	// update lap counter text
+	void UpdateText() {
+		if (P1Lap)
+        {
+			P1Lap.text = string.Format("Lap {0}", lap);		
+		}
+        if (P1Win && lap == 3)
+        {
+            P1Win.text = string.Format("Player 1 Winner");
+        }
+        if (P2Win && lap ==3)
+        {
+            P2Win.text = string.Format("Player 2 Winner");
+        }
+	}
+    public void Update()
+    {
+        if(lap == 3)
+        {
+            CarMovement.acceleration = 0f;
+
+        }
+    }
+	// when lap trigger is entered
+	public void OnLapTrigger(TrackLapTrigger trigger) {
+		if (trigger == next) {
+			if (first == next) {
+				lap++;
+				UpdateText();
+			}
+			SetNextTrigger(next);
+		}
+	}
+	void SetNextTrigger(TrackLapTrigger trigger) {
+		next = trigger.next;
+		SendMessage("OnNextTrigger", next, SendMessageOptions.DontRequireReceiver);
+	}
+}
---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+    public static float acceleration = 0.06f;
+    public float braking = 0.3f;
+    public float steering = 4.0f;
+    private Rigidbody2D rigi;
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    PlayerInputs _inputs;
+    void Start()
+    {
+        _inputs = GetComponent<PlayerInputs>();
+    }
+
+    // update for physics
+    void FixedUpdate()
+    {
+        // steering
+    		float rot = transform.localEulerAngles.z - _inputs.x * steering;
+       		transform.localEulerAngles = new Vector3(0.0f, 0.0f, rot);
+
+        // acceleration/braking
+        		float velocity = rigi.velocity.magnitude;
+        		float y = _inputs.y;
+        		if (y > 0.01f) {
+        			velocity += y * acceleration;
+        		} else if (y < -0.01f) {
+        			velocity += y * braking;
+        		}
+        // apply car movement
+        rigi.velocity = transform.right * velocity;
+       	rigi.angularVelocity = 0.0f;
+    }
+}
